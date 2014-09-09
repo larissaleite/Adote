323d3ad1307f31f554fb58f5f5c2e4f16b22b19f
larissaleite <larissa.leite@gmail.com>
2014-09-09 00:20:58 -0300

    Menu search listing animals' names

--- public/js/app.js
+++ public/js/app.js
@@ -29,10 +29,17 @@ angular.module('adote', ['ngCookies', 'ngResource', 'ngRoute', 'mgcrea.ngStrap']
 
   })
 
-  .controller('MenuCtrl', function($scope) {
+  .controller('MenuCtrl', function($scope, $http) {
 
     $scope.selectedState = "";
     $scope.states = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
     //$scope.selectedAnimal = "";
 
+    $http.get('/api/animals')
+      .success(function(response) {
+        console.log(response);
+        $scope.animals = response;
+      }).error(function(){
+         console.log("erro "+response);
+      });
 });
